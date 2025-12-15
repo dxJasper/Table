2d9ee0a552f3cb9171f1d47a2acf9317c977d3f0
Jasper van der Meijden <jasper.van.der.meijden@dataexcellence.nl>
2025-12-15 15:16:10 +0100

    Add validation

---
+++ Models/ValidationResult.cs
@@ -0,0 +1,31 @@
+namespace Table.Models
+{
+    public class ValidationResult
+    {
+        public bool IsValid { get; set; }
+        public List<string> Errors { get; set; } = new();
+
+        public static ValidationResult Success()
+        {
+            return new ValidationResult { IsValid = true };
+        }
+
+        public static ValidationResult Failure(params string[] errors)
+        {
+            return new ValidationResult
+            {
+                IsValid = false,
+                Errors = errors.ToList()
+            };
+        }
+
+        public static ValidationResult Failure(List<string> errors)
+        {
+            return new ValidationResult
+            {
+                IsValid = false,
+                Errors = errors
+            };
+        }
+    }
+}
